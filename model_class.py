614fc74abf740ee7c722f0783182dfab1c90bd49
Johann Seltmann <johann.seltmann@gmx.net>
2019-07-13 23:27:05 +0200

    build model as class

---
+++ model_class.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+import tensorflow_probability as tfp
+import argparse
+import pickle
+
+## hyperparameters: (originally from Bowman et.al., might change in the future)
+lstm_dim = 191
+z_dim = 13
+embedding_dim = 353
+
+# remove this part once we have preprocessed data
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+#train_dataset, test_dataset = dataset['train'], dataset['test']
+tokenizer = info.features['text'].encoder
+vocab_size = tokenizer.vocab_size
+#BUFFER_SIZE = 10000
+#BATCH_SIZE = 64
+max_len = 25
+start_ind = 19#np.ndarray([19], dtype=np.float32)
+
+
+
+class SentVae(tf.keras.Model):
+
+    def __init__(self):
+        super(SentVae, self).__init__()
+        self.emb_layer = tf.keras.layers.Embedding(vocab_size, embedding_dim, name='emb')
+        #self.inf_lstm = tf.keras.layers.LSTM(lstm_dim, name='inf_out')
+        self.inf_lstm = tf.keras.layers.RNN(cell=tf.keras.layers.LSTMCell(lstm_dim), name='inf_out')
+        self.mu_layer = tf.keras.layers.Dense(z_dim, name='mu')
+        self.sigma_layer = tf.keras.layers.Dense(z_dim, name='sigma')
+
+        self.init_state_layer = tf.keras.layers.Dense(lstm_dim, input_shape=(z_dim,), name='init_state')
+        self.gen_lstm_layer = tf.keras.layers.LSTMCell(lstm_dim, name='gen_lstm_layer')
+        self.state_to_inds = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, inputs, training=False):
+        inf_emb = self.emb_layer(inputs)
+        inf_out = self.inf_lstm(inf_emb)
+
+        mu = self.mu_layer(inf_out)
+        sigma = self.sigma_layer(inf_out)
+
+        dist = tfp.distributions.Normal(loc=0., scale=1.)
+        epsilon = dist.sample(sample_shape=(z_dim,))
+
+        z = tf.add(mu, tf.math.multiply(epsilon, sigma))
+        init_state = self.init_state_layer(z)
+        h, c = init_state, init_state
+
+        if training: # teacher forcing
+            #inp_inds = tf.transpose(inputs, perm=[1,0,2])
+            inp_inds = tf.transpose(inputs, perm=[1,0])
+            inds_out = []
+
+            for ind in tf.unstack(inp_inds):
+                emb = tf.reshape(self.emb_layer(ind), (1,embedding_dim))
+                curr_out, (h,c) = self.gen_lstm_layer(emb, states=[h,c])
+                ind = self.state_to_inds(curr_out)
+                inds_out.append(ind)
+
+            #output = (tf.stack(inds_out, axis=1), mu, sigma)
+            output = tf.stack(inds_out, axis=1)
+        else:
+            emb = tf.reshape(self.emb_layer(start_ind), (1,embedding_dim))
+            inds_out = []
+
+            for _ in range(max_len):
+                curr_out, (h,c) = self.gen_lstm_layer(emb, states=[h,c])
+                new_ind_oh = self.state_to_inds(curr_out)
+                new_ind = tf.argmax(new_ind_oh, axis=1)
+                emb = tf.reshape(self.emb_layer(new_ind), (1,embedding_dim))
+                inds_out.append(new_ind_oh)
+            #output = (tf.stack(inds_out, axis=1), mu, sigma)
+            output = tf.stack(inds_out, axis=1)
+
+        #output = tf.stack(inds_out, axis=1)
+        return output
---
+++ train_class.py
@@ -0,0 +1,117 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+import tensorflow_probability as tfp
+import argparse
+import pickle
+
+from model_class import SentVae
+
+train_data_path = "data/yelp_train.pkl"
+dev_data_path = "data/yelp_dev.pkl"
+word_inds_path = "data/yelp_word_inds.pkl"
+
+## hyperparameters: (originally from Bowman et.al., might change in the future)
+lstm_dim = 191
+z_dim = 13
+embedding_dim = 353
+
+
+# remove this part once we have preprocessed data
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+tokenizer = info.features['text'].encoder
+vocab_size = tokenizer.vocab_size
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+max_len = 25
+
+#with open(train_data_path, "rb") as tdf:
+#    train_data = pickle.load(tdf)
+#with open(dev_data_path, "rb") as ddf:
+#    dev_data = pickle.load(ddf)
+#with open(word_inds_path, "rb") as wif:
+#    word_inds = pickle.load(wif)
+#    word2num = word_inds["word2num"]
+#    num2word = word_inds["num2word"]
+#
+#vocab_size = len(word2num)
+
+
+def to_one_hot(x):
+    true = tf.cast(x, tf.int32)
+    true = tf.one_hot(true, vocab_size)
+    return true
+
+#train_data = [to_one_hot(x) for x in train_data]
+#dev_data = [to_one_hot(x) for x in dev_data]
+
+#train_dataset = train_dataset.map(lambda x,y : ((x[:max_len],x[:max_len]),x[:max_len]))
+train_dataset = train_dataset.map(lambda x,y : (x[:max_len],x[:max_len]))
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+#train_dataset = train_dataset.padded_batch(BATCH_SIZE, (((max_len,),(max_len,)),(max_len,)))
+train_dataset = train_dataset.padded_batch(BATCH_SIZE, ((max_len,),(max_len,)))
+train_dataset = train_dataset.map(lambda x,y : (x,to_one_hot(x)))
+
+test_dataset = test_dataset.map(lambda x,y : (x[:max_len],x[:max_len]))
+test_dataset = test_dataset.shuffle(BUFFER_SIZE)
+test_dataset = test_dataset.padded_batch(BATCH_SIZE, ((max_len,),(max_len,)))
+test_dataset = test_dataset.map(lambda x,y : (x,to_one_hot(x)))
+
+
+vae = SentVae()
+
+
+def vae_loss(true, pred):
+    #true = true[0]
+    #pred_out = pred[0]
+    #mu = pred[1]
+    #sigma = pred[2]
+    reconstruction_loss = tf.keras.losses.MeanSquaredError()(true, pred)
+    reconstruction_loss *= max_len
+
+    #calculate mu and sigma
+    inf_emb = vae.emb_layer(true)[0]
+    inf_out = vae.inf_lstm(inf_emb)
+    mu = vae.mu_layer(inf_out)
+    sigma = vae.sigma_layer(inf_out)
+
+    kl_loss = 1 + sigma - tf.square(mu) - tf.exp(sigma)
+    kl_loss = -0.5 * tf.reduce_sum(kl_loss, 1)
+
+    loss = tf.reduce_mean(reconstruction_loss + kl_loss)
+
+    return loss
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weightsfn", default="model_weights")
+    parser.add_argument("--modelfn", default="model.hd5")
+    parser.add_argument("--logdir", default="logs")
+
+    args = parser.parse_args()
+    weights_filename = args.weightsfn
+    model_filename = args.modelfn
+    logdir = args.logdir
+
+    vae.compile(optimizer=tf.keras.optimizers.Adam(),
+                loss=vae_loss)
+
+    #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+    history = vae.fit(train_dataset,
+                      epochs=1,
+                      validation_data=test_dataset)#,
+                      #callbacks=[tensorboard_callback])
+    #history = va.fit(train_data, train_data,
+    #                  epochs=1,
+    #                  validation_data=(test_dataset,test_dataset))
+
+    #w = inf_net.layers[-1].weights
+    #vae.save_weights(weights_filename + "/vae")
+    #print(inf_net.layers[-1].weights == w)
+
+    #inf_net.save_weights(weights_filename + "/inf_net")
+    #init_state_model.save_weights(weights_filename + "/init_state")
+    vae.save(model_filename)
