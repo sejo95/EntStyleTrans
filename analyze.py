1e52421aefbf93145fd6798b7ac6ba537f76336b
Johann Seltmann <johann.seltmann@gmx.net>
2019-07-17 00:02:45 +0200

    added test sentences

--- analyze.py
+++ analyze.py
@@ -23,24 +23,30 @@ zs = [[0] * 13,
       list(range(13)),
       list(range(13)).reverse()]
 
+sents = ["ok never going back to this place again .",
+         "easter day nothing open , heard about this place figured it would ok .",
+         "the last couple years this place has been going down hill .",
+         "why is the rum gone ?",
+         "there was no water .",
+         "colorless green ideas sleep furiously ."]
 
 
-def test_vae(weights_filename, sents_filename, log_filename):
+
+def test_vae(weights_filename, log_filename):
     vae = SentVae()
     vae.load_weights(weights_filename)
 
     inferred = []
-    with open(sents_filename) as sents_file:
-        with open(log_filename, "w") as log_file:
-            for line in sents_file:
-                mu, sigma = vae.inference(line)
-                z = vae.sample_z(mu, sigma)
-                new_sent = vae.generate(z)
-                log_file.write(line)
-                log_file.write(str(mu) + "   " + str(sigma) + "\n")
-                log_file.write(str(z) + "\n")
-                log_file.write(new_sent + "\n")
-                log_file.write("\n\n")
+    with open(log_filename, "w") as log_file:
+        for sent in sents
+            mu, sigma = vae.inference(sent)
+            z = vae.sample_z(mu, sigma)
+            new_sent = vae.generate(z)
+            log_file.write(sent + "\n")
+            log_file.write(str(mu) + "   " + str(sigma) + "\n")
+            log_file.write(str(z) + "\n")
+            log_file.write(new_sent + "\n")
+            log_file.write("\n\n")
 
 
 def test_generator(weights_filename, log_filename):
